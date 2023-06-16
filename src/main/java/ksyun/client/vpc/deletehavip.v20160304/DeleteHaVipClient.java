b727a835d92d39c7ae9c4805049d5b78e87e95a2
FengYikai <feng_yikai@foxmail.com>
2023-06-16 16:56:34 +0800

    Update SDK

---
+++ src/main/java/ksyun/client/vpc/deletehavip.v20160304/DeleteHaVipClient.java
@@ -0,0 +1,175 @@
+package ksyun.client.vpc.deletehavip.v20160304;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import common.BaseClient;
+import common.Credential;
+import common.utils.HttpClientUtils;
+import common.utils.SignUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+/**
+* @Classname DeleteHaVipClient
+* @Description 删除HaVip
+*/
+@Slf4j
+public class DeleteHaVipClient extends BaseClient {
+    private final static String service = "vpc";
+    private final static String version = "2016-03-04";
+    private final static String action = "DeleteHaVip";
+
+
+    /**
+     * 证书
+     */
+    private Credential credential;
+
+
+    public DeleteHaVipClient(Credential credential) {
+        this.credential = credential;
+    }
+
+
+    /**
+     * post请求
+     *
+     * @param path
+     * @param requestObj
+     * @return
+     * @throws Exception
+     */
+    public DeleteHaVipResponse doPost(String path, DeleteHaVipRequest requestObj) throws Exception {
+        return doPost(path, requestObj, null);
+    }
+
+    /**
+     * post请求
+     *
+     * @param path
+     * @param requestObj
+     * @param head
+     * @return
+     * @throws Exception
+     */
+    public DeleteHaVipResponse doPost(String path, DeleteHaVipRequest requestObj, Map<String, String> head) throws Exception {
+        JSONObject requestParams = getRequestParams(requestObj);
+        String response = HttpClientUtils.httpPost(path, requestParams, head);
+        log.info("doPost end,path:{},params:{},head:{}", path, requestParams, head);
+        return JSON.parseObject(response, DeleteHaVipResponse.class);
+    }
+
+
+    /**
+     * get 请求
+     *
+     * @param path
+     * @param requestObj
+     * @return
+     * @throws Exception
+     */
+    public DeleteHaVipResponse doGet(String path, DeleteHaVipRequest requestObj) throws Exception {
+        return doGet(path, requestObj, null);
+    }
+
+    /**
+     * doDelete 请求
+     *
+     * @param path
+     * @param requestObj
+     * @return
+     * @throws Exception
+     */
+    public DeleteHaVipResponse doDelete(String path, DeleteHaVipRequest requestObj) throws Exception {
+        return doDelete(path, requestObj, null);
+    }
+
+    /**
+     * doDelete 请求
+     *
+     * @param path
+     * @param requestObj
+     * @param head
+     * @return
+     * @throws Exception
+     */
+    public DeleteHaVipResponse doDelete(String path, DeleteHaVipRequest requestObj, Map<String, String> head) throws Exception {
+        JSONObject requestParams = getRequestParams(requestObj);
+        String response = HttpClientUtils.httpDelete(path, requestParams, head);
+        log.info("doDelete end,path:{},params:{},head:{}", path, requestParams, head);
+        DeleteHaVipResponse DeleteHaVipResponse = JSON.parseObject(response, DeleteHaVipResponse.class);
+        return DeleteHaVipResponse;
+    }
+
+
+    /**
+     * doPut 请求
+     *
+     * @param path
+     * @param requestObj
+     * @return
+     * @throws Exception
+     */
+    public DeleteHaVipResponse doPut(String path, DeleteHaVipRequest requestObj) throws Exception {
+        return doPut(path, requestObj, null);
+    }
+
+    /**
+     * doPut 请求
+     *
+     * @param path
+     * @param requestObj
+     * @param head
+     * @return
+     * @throws Exception
+     */
+    public DeleteHaVipResponse doPut(String path, DeleteHaVipRequest requestObj, Map<String, String> head) throws Exception {
+        JSONObject requestParams = getRequestParams(requestObj);
+        String response = HttpClientUtils.httpPut(path, requestParams, head);
+        log.info("httpPut end,path:{},params:{},head:{}", path, requestParams, head);
+        DeleteHaVipResponse DeleteHaVipResponse = JSON.parseObject(response, DeleteHaVipResponse.class);
+        return DeleteHaVipResponse;
+    }
+
+    /**
+     * get 请求
+     *
+     * @param path
+     * @param requestObj
+     * @param head
+     * @return
+     * @throws Exception
+     */
+    public DeleteHaVipResponse doGet(String path, DeleteHaVipRequest requestObj, Map<String, String> head) throws Exception {
+        JSONObject requestParams = getRequestParams(requestObj);
+        String response = HttpClientUtils.httpGet(path, requestParams, head);
+        log.info("doGet end,path:{},params:{},head:{}", path, requestParams, head);
+        return JSON.parseObject(response, DeleteHaVipResponse.class);
+    }
+
+
+    /**
+     * 构造请求参数
+     *
+     * @param requestObj
+     * @return
+     */
+    private JSONObject getRequestParams(DeleteHaVipRequest requestObj) throws Exception {
+        JSONObject requestParams = new JSONObject();
+        //设置证书
+        getCommonParams(credential, requestParams);
+        //设置接口属性
+        requestParams.put("Service", service);
+        requestParams.put("Action", action);
+        requestParams.put("Version", version);
+
+        //设置请求体请求参数
+        setRequestField(requestObj,requestParams);
+
+        //签名
+        String signature = SignUtils.signature(requestParams, credential.getSignStr());
+        requestParams.put("Signature", signature);
+        return requestParams;
+    }
+}
