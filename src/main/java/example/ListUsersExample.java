afafffd52dd66aaa8b4029ebe0932affce2bf674
FengYikai <feng_yikai@foxmail.com>
2023-08-11 15:25:36 +0800

    Update SDK

--- src/main/java/example/ListUsersExample.java
+++ src/main/java/example/ListUsersExample.java
@@ -17,24 +17,24 @@ import java.util.Properties;
 @Slf4j
 public class ListUsersExample {
     public static void main(String[] args) {
-        //证书
-        Credential credential = new Credential("secretKey", "signStr", "region");
-        ListUsersClient client = new ListUsersClient(credential);
-        ListUsersRequest request = new ListUsersRequest();
-        request.setMarker("1");
-        request.setMaxItems("2");
-        String host = "";
-        try {
-            ListUsersResponse ListUsersResponsePostRaw = client.doPostRaw(host, request);
-            log.info("ListUsers result:{}", JSON.toJSON(ListUsersResponsePostRaw));
-
-            //ListUsersResponse ListUsersResponsePost = client.doPost(host, request);
-            //log.info("ListUsers result:{}", JSON.toJSON(ListUsersResponsePost));
-
-            //ListUsersResponse ListUsersResponseGet = client.doGet(host, request);
-            //log.info("ListUsers result:{}", JSON.toJSON(ListUsersResponseGet));
-        } catch (Exception e) {
-            log.error("ListUsers occur error", e);
-        }
+//        //证书
+//        Credential credential = new Credential("secretKey", "signStr", "region");
+//        ListUsersClient client = new ListUsersClient(credential);
+//        ListUsersRequest request = new ListUsersRequest();
+//        request.setMarker("1");
+//        request.setMaxItems("2");
+//        String host = "";
+//        try {
+//            ListUsersResponse ListUsersResponsePostRaw = client.doPostRaw(host, request);
+//            log.info("ListUsers result:{}", JSON.toJSON(ListUsersResponsePostRaw));
+//
+//            //ListUsersResponse ListUsersResponsePost = client.doPost(host, request);
+//            //log.info("ListUsers result:{}", JSON.toJSON(ListUsersResponsePost));
+//
+//            //ListUsersResponse ListUsersResponseGet = client.doGet(host, request);
+//            //log.info("ListUsers result:{}", JSON.toJSON(ListUsersResponseGet));
+//        } catch (Exception e) {
+//            log.error("ListUsers occur error", e);
+//        }
     }
 }
