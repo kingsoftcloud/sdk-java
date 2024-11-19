48c358691409bccb69d6126e467489cde5d7b34e
fengyikai <fengyikai@kingsoft.com>
2024-11-19 10:14:32 +0800

    Update SDK

---
+++ src/main/java/ksyun/client/epc/activatehotstandbyepc.v20151101/ActivateHotStandbyEpcResponse.java
@@ -0,0 +1,26 @@
+package ksyun.client.epc.activatehotstandbyepc.v20151101;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import common.BaseResponseModel;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Set;
+
+/**
+* @Classname ActivateHotStandbyEpcResponse
+* @Description ActivateHotStandbyEpc 返回体
+*/
+@Data
+@ToString
+public class ActivateHotStandbyEpcResponse extends BaseResponseModel {
+
+    /**
+     * 请求id
+     */
+    @JsonProperty("RequestId")
+    private String requestId;
+
+    //返回结果，需要按需扩展
+
+}
