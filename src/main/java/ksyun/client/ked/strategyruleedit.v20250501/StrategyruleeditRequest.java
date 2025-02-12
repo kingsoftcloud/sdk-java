2c3f174b94ae9c82e9ea385e0502c99895dd3e96
fengyikai <fengyikai@kingsoft.com>
2025-02-12 16:37:54 +0800

    Update SDK

--- src/main/java/ksyun/client/ked/strategyruleedit.v20250501/StrategyruleeditRequest.java
+++ src/main/java/ksyun/client/ked/strategyruleedit.v20250501/StrategyruleeditRequest.java
@@ -16,11 +16,39 @@ import java.util.Arrays;
 public class StrategyruleeditRequest{
     /**安全组出站规则*/
     @KsYunField(name="policies")
+    private PoliciesDto Policies;
+    @Data
+    @ToString
+    public static class PoliciesDto {
+        /**规则描述*/
+        @KsYunField(name="description")
+        private String Description;
+
+        /**out：流出
+         暂不允许流入规则添加*/
+        @KsYunField(name="direction")
+        private String Direction;
+
+        /**ip 值*/
+        @KsYunField(name="cidrBlock")
+        private String CidrBlock;
+
+        /**最大端口号，0 表示所有端口*/
+        @KsYunField(name="maxPortRange")
+        private String MaxPortRange;
+
+        /**最小端口号，0 标识所有端口*/
+        @KsYunField(name="minPortRange")
+        private String MinPortRange;
+
+        /**固定值 ANY*/
+        @KsYunField(name="protocol")
+        private String Protocol;
+    }
+
 
     /**策略组 id
 */
     @KsYunField(name="securityGroupId")
     private String SecurityGroupId;
-
-
 }
