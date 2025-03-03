a797194b358cf5e87df84e667820e9f8fb34b1c1
fengyikai <fengyikai@kingsoft.com>
2025-03-03 18:52:41 +0800

    Update SDK

--- src/main/java/ksyun/client/kec/createscalinggroup.v20160304/CreateScalingGroupRequest.java
+++ src/main/java/ksyun/client/kec/createscalinggroup.v20160304/CreateScalingGroupRequest.java
@@ -87,7 +87,6 @@ public class CreateScalingGroupRequest {
          */
         @KsYunField(name = "ServerPort")
         private List<Integer> ServerPortList;
-        private List<Int> ServerPortList;
         /**
          * 端口权重
          */
