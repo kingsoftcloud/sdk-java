d042417bcaa01f8fef874be236c1180a571add81
fengyikai <fengyikai@kingsoft.com>
2025-10-20 10:23:10 +0800

    CDN:更新GetDomainLogs

---
+++ src/main/java/ksyun/client/cdn/getdomainlogs.v20250503/GetDomainLogsResponse.java
@@ -0,0 +1,26 @@
+package ksyun.client.cdn.getdomainlogs.v20250503;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import common.BaseResponseModel;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Set;
+
+/**
+* @Classname GetDomainLogsResponse
+* @Description GetDomainLogs 返回体
+*/
+@Data
+@ToString
+public class GetDomainLogsResponse extends BaseResponseModel {
+
+    /**
+     * 请求id
+     */
+    @JsonProperty("RequestId")
+    private String requestId;
+
+    //返回结果，需要按需扩展
+
+}
