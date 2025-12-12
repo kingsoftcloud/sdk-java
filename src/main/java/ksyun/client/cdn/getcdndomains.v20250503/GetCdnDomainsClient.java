aacd148a1b76784a2cde7377d1676818d5fd6a18
fengyikai <fengyikai@kingsoft.com>
2025-12-12 17:43:56 +0800

    CDN

--- src/main/java/ksyun/client/cdn/getcdndomains.v20250503/GetCdnDomainsClient.java
+++ src/main/java/ksyun/client/cdn/getcdndomains.v20250503/GetCdnDomainsClient.java
@@ -65,7 +65,6 @@ public class GetCdnDomainsClient extends BaseClient {
 
     /**
      * post 请求
-     *
      * @param path
      * @param requestObj
      * @return
@@ -91,7 +90,6 @@ public class GetCdnDomainsClient extends BaseClient {
         String response = doRpc(path, requestObj, requestHeaders, "post");
         return JSON.parseObject(response, GetCdnDomainsResponse.class);
     }
-
     /**
      * get 请求
      *
@@ -178,14 +176,14 @@ public class GetCdnDomainsClient extends BaseClient {
      */
     public GetCdnDomainsResponse doPut(String path, GetCdnDomainsRequest requestObj, Map<String, String> head) throws Exception {
         final Map<String, String> requestHeaders = head != null ? new HashMap<>(head) : new HashMap<>();
-        requestHeaders.putIfAbsent("Content-Type", "application/x-www-form-urlencoded");
+       requestHeaders.putIfAbsent("Content-Type", "application/x-www-form-urlencoded");
         String response = doRpc(path, requestObj, requestHeaders, "put");
         return JSON.parseObject(response, GetCdnDomainsResponse.class);
     }
 
     /**
-     * rpc
-     *
+         * rpc
+         *
      * @param path
      * @param requestObj
      * @param head
--- src/main/java/ksyun/client/cdn/getcdndomains.v20250503/GetCdnDomainsRequest.java
+++ src/main/java/ksyun/client/cdn/getcdndomains.v20250503/GetCdnDomainsRequest.java
@@ -14,4 +14,46 @@ import java.util.Arrays;
  */
 @Data
 public class GetCdnDomainsRequest {
+    /**
+     * 分页大小，默认20，最大500，取值1～500间整数
+     */
+    @KsYunField(name = "PageSize")
+    private Integer PageSize;
+
+    /**
+     * 取第几页。默认为1，取值1～10000
+     */
+    @KsYunField(name = "PageNumber")
+    private Integer PageNumber;
+
+    /**
+     * 按域名过滤，默认为空，为空时代表当前用户下所有域名，域名长度最大255，不支持多个域名同时查询
+     */
+    @KsYunField(name = "DomainName")
+    private String DomainName;
+
+    /**
+     * 查询指定项目下的域名。默认为空，为空时查询当前用户下所有域名ProjectId可至金山云控制台-资源管理-项目管理查询
+     */
+    @KsYunField(name = "ProjectId")
+    private Integer ProjectId;
+
+    /**
+     * 按域名状态过滤。默认为空，为空时查询当前用户下所有域名的全部状态，取值为：online：正在运行；offline：已停止；configuring：配置中；configure_failed：配置失败 ；icp_checking：审核中；icp_check_failed：审核失败；locked：已封禁；locking：封禁中
+     */
+    @KsYunField(name = "DomainStatus")
+    private String DomainStatus;
+
+    /**
+     * 产品类型：file：大文件下载，video：音视频点播，page：图片小文件，wcdn：全站加速，默认为空，代表当前用户下全部产品类型（包括wcdn产品，不支持live：流媒体直播），支持同时查询多个产品类型，两个类型之间用英文逗号（半角）隔开
+     */
+    @KsYunField(name = "CdnType")
+    private String CdnType;
+
+    /**
+     * 域名过滤是否使用模糊匹配，取值为on：开启，off：关闭，默认为onon
+     */
+    @KsYunField(name = "FuzzyMatch")
+    private String FuzzyMatch;
+
 }
