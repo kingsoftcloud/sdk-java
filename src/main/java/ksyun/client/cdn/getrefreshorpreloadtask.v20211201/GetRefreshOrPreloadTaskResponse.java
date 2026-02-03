c6fd617228f3a12a031022fe5c8c3c75fb11fc2b
fengyikai <fengyikai@kingsoft.com>
2026-02-03 15:44:54 +0800

    内容分发网络(cdn)：更新GetRefreshOrPreloadTask参数

--- src/main/java/ksyun/client/cdn/getrefreshorpreloadtask.v20211201/GetRefreshOrPreloadTaskResponse.java
+++ src/main/java/ksyun/client/cdn/getrefreshorpreloadtask.v20211201/GetRefreshOrPreloadTaskResponse.java
@@ -15,8 +15,74 @@ import java.util.List;
 @ToString
 public class GetRefreshOrPreloadTaskResponse extends BaseResponseModel {
 
-    /***/
-    @JsonProperty("GetRefreshOrPreloadTaskResponse")
-    private String GetRefreshOrPreloadTaskResponse;
+    /**开始时间*/
+    @JsonProperty("StartTime")
+    private String StartTime;
+
+    /**结束时间*/
+    @JsonProperty("EndTime")
+    private String EndTime;
+
+    /**查询的URL列表*/
+    @JsonProperty("Urls")
+    private List<UrlsDto> Urls;
+
+    @Data
+    @ToString
+    public static class UrlsDto {
+        /**需要查询进度的url*/
+        @JsonProperty("Url")
+        private String Url;
+
+    }
+
+    /**整页大小*/
+    @JsonProperty("PageSize")
+    private Long PageSize;
+
+    /**页码*/
+    @JsonProperty("PageNumber")
+    private Long PageNumber;
+
+    /**Url总条数*/
+    @JsonProperty("TotalCount")
+    private Long TotalCount;
+
+    /**刷新或预热任务进度百分比及状态信息*/
+    @JsonProperty("Datas")
+    private List<DatasDto> Datas;
+
+    @Data
+    @ToString
+    public static class DatasDto {
+        /**任务类别，取值为：refresh，刷新任务；取值为:preload，预热任务*/
+        @JsonProperty("Type")
+        private String Type;
+
+        /**任务的细分类型，取值为：REFRESH_FILE表示URL刷新，REFRESH_DIR表示目录刷新，PRELOAD_FILE表示URL预热。注：PRELOAD_FILE不在请求传参SubType的取值范围里，单纯作为响应的类型值*/
+        @JsonProperty("SubType")
+        private String SubType;
+
+        /**刷新或预热的Url地址*/
+        @JsonProperty("Url")
+        private String Url;
+
+        /**刷新或预热任务进度百分比数值*/
+        @JsonProperty("Progress")
+        private Double Progress;
+
+        /**刷新或预热状态信息，取值分别是success(成功)、progressing(进行中)、failed(刷新失败)*/
+        @JsonProperty("Status")
+        private String Status;
+
+        /**任务ID，按照任务ID查询刷新或预热任务进度百分比及状态信息*/
+        @JsonProperty("TaskId")
+        private String TaskId;
+
+        /**任务创建时间*/
+        @JsonProperty("CreateTime")
+        private String CreateTime;
+
+    }
 
 }
